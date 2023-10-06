f6abb00dd271efd1ffc8099ce9db1e3480c09f8d
Krushna92 <root@ip-172-31-82-27.ec2.internal>
2023-10-06 06:33:55 +0000

    myprojectagainplease

--- javanew.java
+++ javanew.java
@@ -2,7 +2,7 @@ class Simple{
 	
     public static void main(String args[]){  
 	
-     System.out.println("Hello again Java");  
+     System.out.println("Hello again Java i got you");  
 	
     }  
 	
